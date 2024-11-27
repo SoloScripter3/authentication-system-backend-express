47868cf69fa1ab5c7adba5c15deb1935be176280
SoloScripter3 <vardhanv6259@gmail.com>
2024-11-27 11:32:12 +0530

    added boilerplate

---
+++ backend/src/server.js
@@ -0,0 +1,9 @@
+require("dotenv").config();
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => res.json("hello"));
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`http://localhost:${port}`));

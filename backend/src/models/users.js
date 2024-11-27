ad6627bca29fece56a6d5a9d2583010d88df2d89
SoloScripter3 <vardhanv6259@gmail.com>
2024-11-27 17:42:57 +0530

    updated schema

--- backend/src/models/users.js
+++ backend/src/models/users.js
@@ -11,6 +11,10 @@ const userSchema = new mongoose.Schema({
     required: true,
     unique: true,
   },
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
   password: {
     type: String,
     required: true,

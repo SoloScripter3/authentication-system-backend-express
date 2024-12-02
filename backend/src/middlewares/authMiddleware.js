11552cdcad553f477f36ae5e7bd8198ee935dbc9
SoloScripter3 <vardhanv6259@gmail.com>
2024-12-02 16:44:53 +0530

    added authMiddleware for secure routes

---
+++ backend/src/middlewares/authMiddleware.js
@@ -0,0 +1,21 @@
+const jwt = require("jsonwebtoken");
+
+const authMiddleware = (req, res, next) => {
+  const authHeaders = req.headers["authorization"];
+  if (!authHeaders) {
+    return res.json({ message: "Denied" });
+  }
+  const token = authHeaders.split(" ")[1];
+  if (!token) {
+    return res.json({ message: "Forbidden" });
+  }
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+    req.user = decoded.userId;
+    next();
+  } catch (err) {
+    return res.json({ message: "Invalid token" });
+  }
+};
+
+module.exports = authMiddleware;
